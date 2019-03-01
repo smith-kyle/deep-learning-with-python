4ae2f28a412685d1e332f6d87351ebf89f9eb296
Kyle Smith <kyles@axosoft.com>
2019-02-28 19:29:03 -0700

    First take at training cats-vs-dogs

--- chapter5.py
+++ chapter5.py
@@ -1,18 +1,64 @@
 import os
 import shutil
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+
+def plot_history(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
+
+
+def get_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    return model
+
+
+original_dataset_dir = './data/dogs-vs-cats'
+base_dir = './data/cats-and-dogs-small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
 
 
 def prep_training_dirs():
-    original_dataset_dir = './data/dogs-vs-cats'
 
-    base_dir = './data/cats-and-dogs-small'
     os.mkdir(base_dir)
 
-    train_dir = os.path.join(base_dir, 'train')
     os.mkdir(train_dir)
-    validation_dir = os.path.join(base_dir, 'validation')
     os.mkdir(validation_dir)
-    test_dir = os.path.join(base_dir, 'test')
     os.mkdir(test_dir)
 
     train_cats_dir = os.path.join(train_dir, 'cats')
@@ -71,3 +117,33 @@ def prep_training_dirs():
 
 
 # prep_training_dirs()
+model = get_model()
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=1e-4), metrics=['acc'])
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+
+validation_generator = train_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50
+)
+
+model.save('cats_and_dogs_small_1.h5')
